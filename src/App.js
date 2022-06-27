de19afc4364804d17544c9fe9c5ddbff1fa8a7a5
felitommasi <felipe.tommasi@gmail.com>
2022-06-26 23:28:29 -0300

    card

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
-import { VStack } from '@chakra-ui/react'
+import { VStack } from "@chakra-ui/react";
 import NavBar from "./components/NavBar/NavBar";
-
+import Card from "./components/Card/Card";
 
 function App() {
 	return (
-			<div className="App">
-				<NavBar />
+		<div className="App">
+			<NavBar />
 
-				<VStack>
-					<ItemListContainer greeting="Bienvenido a mi ecommerce" />
-				</VStack>
-			</div>
+			<VStack>
+				<Card />
+			</VStack>
+		</div>
 	);
 }
 
--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -1,37 +1,54 @@
-import React, { useState, useEffect } from 'react';
-import { Button, HStack, VStack } from '@chakra-ui/react'
+import React, { useState, useEffect } from "react";
+import { Button, HStack, VStack } from "@chakra-ui/react";
 import { MdShoppingCart } from "react-icons/md";
 
-export const  ItemCount = ({ initial, stock, onAdd }) => {
-  const [count, setCount] = useState(parseInt(initial));
+export const ItemCount = ({ initial, stock, onAdd }) => {
+	const [count, setCount] = useState(parseInt(initial));
 
-  const decreaseCount = () => {
-    setCount(count - 1);
-  };
+	const decreaseCount = () => {
+		setCount(count - 1);
+	};
 
-  const increaseCount = () => {
-    setCount(count + 1);
-  };
+	const increaseCount = () => {
+		setCount(count + 1);
+	};
 
-  useEffect(() => {
-    setCount(parseInt(initial));
-  }, [initial]);
+	useEffect(() => {
+		setCount(parseInt(initial));
+	}, [initial]);
 
-  return (
-      <VStack>
-      
-      <HStack spacing='32px' mb='16px'>
-        <Button disabled={count <= 0} onClick={decreaseCount} colorScheme='gray'>-</Button>
-          <span>{count}</span>
-        <Button disabled={count >= stock} onClick={increaseCount} colorScheme='gray'>+</Button>
-      </HStack>
-   
-        <div>
-          <Button rightIcon={<MdShoppingCart/>} disabled={stock <= 0} onClick={() => onAdd(count)} colorScheme='blue'>Agregar al carrito</Button>
-        </div>
-      
-      </VStack>
-    );
+	return (
+		<VStack>
+			<HStack spacing="40px" mb="16px" bg="white" rounded="8px" p={1}>
+				<Button
+					disabled={count <= 0}
+					onClick={decreaseCount}
+					colorScheme="gray"
+					boxShadow="sm"
+				>
+					-
+				</Button>
+				<span>{count}</span>
+				<Button
+					disabled={count >= stock}
+					onClick={increaseCount}
+					colorScheme="gray"
+					boxShadow="sm"
+				>
+					+
+				</Button>
+			</HStack>
+
+			<Button
+				rightIcon={<MdShoppingCart />}
+				disabled={stock <= 0}
+				onClick={() => onAdd(count)}
+				colorScheme="blue"
+			>
+				Agregar al carrito
+			</Button>
+		</VStack>
+	);
 };
 
 export default ItemCount;
