a4dee9f8d2082ca54b0ee1338db3d4b3cefd1205
felitommasi <felipe.tommasi@gmail.com>
2022-08-09 22:02:03 -0300

    fix errors form

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,134 +1,110 @@
-import React, { useContext } from "react";
-import { useForm } from "react-hook-form";
+import React, { useContext, useState } from "react";
+import { collection, addDoc } from "firebase/firestore";
+import { CartContext } from "../../context/CartContext";
 import {
-	FormErrorMessage,
 	FormLabel,
 	FormControl,
 	Input,
 	Button,
 	Box,
 } from "@chakra-ui/react";
-import { getFirestore, collection, addDoc } from "firebase/firestore";
-import { CartContext } from "../../context/CartContext";
+import { db } from "../../firebase/firebase";
 import Swal from "sweetalert2";
 
 function Checkout() {
-	const {
-		handleSubmit,
-		register,
-		formState: { errors, isSubmitting },
-	} = useForm();
-
 	const { products, totalPrice } = useContext(CartContext);
+	const [order, setOrder] = useState(); //order
+	const [orderId, setOrderId] = useState(); //id
 
-	function onSubmit(customer) {
-		console.log(customer);
-		const db = getFirestore();
-		const ordersCollection = collection(db, "orders");
+	async function onSubmit(e) {
+		e.preventDefault();
 
-		const order = {
-			name: customer.name,
-			lastName: customer.lastName,
-			email: customer.email,
-			phone: customer.phone,
-			items: products.map((item) => ({
-				id: item.id,
-				title: item.title,
-				price: item.price,
-				quantity: item.quantity,
-			})),
+		let newOrder = {
+			buyer: {
+				name: e.target[0].value,
+				email: e.target[1].value,
+				phone: e.target[2].value,
+			},
+			items: products,
 			total: totalPrice(),
 		};
-		if ("orders" !== "") {
-			addDoc(ordersCollection, order)
-            .then(({ id }) => console.log(id));
-		} else {
-			console.log("Invalid data");
+		setOrder(newOrder);
+
+		try {
+			if (
+				newOrder.buyer.name !== "" &&
+				newOrder.buyer.email !== "" &&
+				newOrder.buyer.phone !== ""
+			) {
+				const queryCollection = collection(db, "orders");
+				const docRef = await addDoc(queryCollection, order);
+				const docRefId = docRef.id;
+				setOrderId(docRefId);
+			} else {
+				Swal.fire({
+					icon: "warning",
+					text: "Please complete all the form fields",
+				});
+			}
+		} catch (error) {
+			console.log(error);
 		}
 	}
 
-	return (
-		<Box p={[6, 8, 10, 12]}>
-			<form onSubmit={handleSubmit(onSubmit)}>
-				<FormControl isInvalid={errors.name}>
-					<FormLabel htmlFor="name">First name</FormLabel>
-					<Input
-						mb={5}
-						id="name"
-						placeholder="Juan"
-						{...register("name", {
-							required: "This field is required",
-							minLength: { value: 4, message: "Minimum length should be 4" },
-						})}
-					/>
-					<FormErrorMessage>
-						{errors.name && errors.name.message}
-					</FormErrorMessage>
-				</FormControl>
-				<FormControl isInvalid={errors.lastName}>
-					<FormLabel htmlFor="lastName">Last name</FormLabel>
-					<Input
-						mb={5}
-						id="lastName"
-						placeholder="Perez"
-						{...register("lastName", {
-							required: "This field is required",
-							minLength: { value: 4, message: "Minimum length should be 4" },
-						})}
-					/>
-					<FormErrorMessage>
-						{errors.lastName && errors.lastName.message}
-					</FormErrorMessage>
-				</FormControl>
-				<FormControl isInvalid={errors.email}>
-					<FormLabel htmlFor="email">E-mail</FormLabel>
-					<Input
-						mb={5}
-						id="email"
-						placeholder="asdf@gmail.com"
-						{...register("email", {
-							required: "This field is required",
-							pattern: {
-								value: /^\S+@\S+$/i,
-								message: "Please enter a valid email",
-							},
-						})}
-					/>
-					<FormErrorMessage>
-						{errors.email && errors.email.message}
-					</FormErrorMessage>
-				</FormControl>
-				<FormControl isInvalid={errors.phone}>
-					<FormLabel htmlFor="phone">Phone</FormLabel>
-					<Input
-						mb={5}
-						id="phone"
-						placeholder="11-5983-0283"
-						{...register("phone", {
-							required: "This field is required",
-							minLength: { value: 6, message: "Minimum length should be 6" },
-							maxLength: { value: 12, message: "Max length should be 12" },
-							pattern: {
-								value: /^[+]*[(]{0,1}[0-9]{1,3}[)]{0,1}[-s./0-9]*$/g,
-								message: "please enter a valid phone number",
-							},
-						})}
-					/>
-					<FormErrorMessage>
-						{errors.phone && errors.phone.message}
-					</FormErrorMessage>
-				</FormControl>
-				<Button
-					mt={4}
-					colorScheme="teal"
-					isLoading={isSubmitting}
-					type="submit"
-				>
-					Place order
-				</Button>
-			</form>
-		</Box>
-	);
+	const buySuccess = () => {
+		setTimeout(() => {
+			Swal.fire({
+				icon: "success",
+				title: "Your purchase is on the way! Tracking number:",
+				text: orderId,
+			});
+		}, 1000);
+
+		return (
+			<>
+				{orderId ? (
+					buySuccess()
+				) : (
+					<Box p={[6, 8, 10, 12]}>
+						<form onSubmit={onSubmit}>
+							<FormControl>
+								<FormLabel htmlFor="name">First name</FormLabel>
+								<Input
+									mb={5}
+									id="name"
+									placeholder="Juan"
+								/>
+							</FormControl>
+							<FormControl>
+								<FormLabel htmlFor="lastName">Last name</FormLabel>
+								<Input
+									mb={5}
+									id="lastName"
+									placeholder="Perez"
+								/>
+							</FormControl>
+							<FormControl>
+								<FormLabel htmlFor="email">E-mail</FormLabel>
+								<Input
+									mb={5}
+									id="email"
+									placeholder="juanperez@gmail.com"
+								/>
+							</FormControl>
+							<Button
+								mt={4}
+								colorScheme="teal"
+								type="submit"
+							>
+								Place order
+							</Button>
+						</form>
+					</Box>
+				)}
+				;
+			</>
+		);
+	};
 }
 
 export default Checkout;
